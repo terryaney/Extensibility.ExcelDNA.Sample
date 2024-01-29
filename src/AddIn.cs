18d8c9c1120d191393717eab6e08d507f7f06ba1
Terry Aney <terry.aney@icloud.com>
2024-01-29 08:31:23 -0600

    Initial commit

---
+++ src/AddIn.cs
@@ -0,0 +1,30 @@
+﻿using ExcelDna.Integration;
+using ExcelDna.Registration;
+
+namespace KAT.Extensibility.Excel.AddIn;
+
+public class AddIn : IExcelAddIn
+{
+	public void AutoOpen()
+	{
+		RegisterFunctions();
+	}
+
+	public void AutoClose()
+	{
+	}
+
+	public void RegisterFunctions()
+	{
+		ExcelRegistration
+			.GetExcelFunctions()
+			.Select( UpdateHelpTopic )
+			.RegisterFunctions();
+	}
+
+	public ExcelFunctionRegistration UpdateHelpTopic( ExcelFunctionRegistration funcReg )
+	{
+		funcReg.FunctionAttribute.HelpTopic = "http://www.bing.com";
+		return funcReg;
+	}
+}
---
+++ src/Ribbon.cs
@@ -0,0 +1,38 @@
+﻿using ExcelDna.Integration.CustomUI;
+using ExcelDna.Integration.Extensibility;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace KAT.Extensibility.Excel.AddIn;
+
+[ComVisible( true )]
+public class Ribbon : ExcelRibbon
+{
+	public override string GetCustomUI( string RibbonID )
+	{
+		var resourceName = "KAT.Extensibility.Excel.AddIn.Resources.Ribbon.xml";
+		var assembly = Assembly.GetExecutingAssembly();
+		using var stream = assembly.GetManifestResourceStream( resourceName )!;
+		using var reader = new StreamReader( stream );
+		return reader.ReadToEnd();
+	}
+
+	public override void OnConnection( object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom )
+	{
+		// TODO: Need to hook up event handlers...
+
+		base.OnConnection( Application, ConnectMode, AddInInst, ref custom );
+	}
+
+	public override void OnDisconnection( ext_DisconnectMode RemoveMode, ref Array custom )
+	{
+		// TODO: Need to unhook event handlers...
+		
+		base.OnDisconnection( RemoveMode, ref custom );
+	}
+
+	public void OnButtonPressed( IRibbonControl control )
+	{
+		MessageBox.Show( "Hello from control " + control.Id );
+	}
+}
