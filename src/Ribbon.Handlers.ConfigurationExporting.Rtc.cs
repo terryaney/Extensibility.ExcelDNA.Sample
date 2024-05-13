ad61236fb84af291d1a4c4085022dc7908ebfded
Terry Aney <terry.aney@icloud.com>
2024-05-13 18:31:02 -0500

    RTC Export Finished

---
+++ src/Ribbon.Handlers.ConfigurationExporting.Rtc.cs
@@ -0,0 +1,198 @@
+﻿using System.Text.Json.Nodes;
+using KAT.Camelot.Domain.Extensions;
+using KAT.Camelot.RBLe.Core.Calculations;
+using MSExcel = Microsoft.Office.Interop.Excel;
+
+namespace KAT.Camelot.Extensibility.Excel.AddIn;
+
+public partial class Ribbon
+{
+	private void ExportRtcData()
+	{
+		var workbook = application.ActiveWorkbook!;
+		var fileName = @"C:\BTR\Camelot.Old\Websites\Personal\RTC\rtcsettings.json";
+		var sheets = workbook.Sheets.Cast<MSExcel.Worksheet>();
+
+		var clubInfo = sheets.First( s => s.Name == "Club Info" );
+		var indoorStartDate = clubInfo.Range[ "B1" ].GetText();
+		var indoorHours = clubInfo.Range[ "A2" ];
+		var itemCosts = indoorHours.End[ MSExcel.XlDirection.xlDown ].End[ MSExcel.XlDirection.xlDown ];
+		var indoorDues = itemCosts.End[ MSExcel.XlDirection.xlDown ].End[ MSExcel.XlDirection.xlDown ];
+		var outdoorStartDate = indoorDues.End[ MSExcel.XlDirection.xlDown ].End[ MSExcel.XlDirection.xlDown ].Offset[ 0, 1 ].GetText();
+		var outdoorHours = indoorDues.End[ MSExcel.XlDirection.xlDown ].End[ MSExcel.XlDirection.xlDown ].Offset[ 1 ];
+		var outdoorDues = outdoorHours.End[ MSExcel.XlDirection.xlDown ].End[ MSExcel.XlDirection.xlDown ];
+
+		// First Hours Of Operation
+		var settings = new JsonObject
+		{
+			{ "RTCSettings", new JsonObject
+				{
+					{ "Indoor", new JsonObject
+						{
+							{ "StartDate", !string.IsNullOrEmpty( indoorStartDate ) ? indoorStartDate : null },
+							{ "HoursOfOperation", GetKeyValueArrays( indoorHours.Offset[ 1 ] ).ToJsonArray() }
+						}
+						.AddProperties( new [] { new JsonKeyProperty( "MembershipDues", GetKeyValueArrays( indoorDues.Offset[ 1 ] ).ToJsonArray() ) } )
+						.AddProperties( GetPropertyValueArrays( itemCosts.Offset[ 1 ] ) )
+					},
+					{ "Outdoor", new JsonObject
+						{
+							{ "StartDate", !string.IsNullOrEmpty( outdoorStartDate ) ? outdoorStartDate : null },
+							{ "HoursOfOperation", GetKeyValueArrays( outdoorHours.Offset[ 1 ] ).ToJsonArray() },
+							{ "MembershipDues", GetKeyValueArrays( outdoorDues.Offset[ 1 ] ).ToJsonArray() }
+						}
+					},
+					{ "Programs", (
+						from sheet in sheets.Where( s => s.Name != "Club Info" && s.Name != "Schedule.Template" && s.Name != "Schedule_Template" )
+						select GetSchedules( sheet )
+					).ToJsonArray() }
+				}
+			}
+		};
+
+		File.WriteAllText( fileName, JsonExtensions.ToJsonString( settings, writeIndented: true, ignoreNulls: true ) );
+	}
+
+	private static IEnumerable<JsonObject> GetKeyValueArrays( MSExcel.Range start )
+	{
+		var currentRow = 0;
+		string key;
+
+		while ( !string.IsNullOrEmpty( key = start.Offset[ currentRow ].GetText() ) )
+		{
+			yield return
+				new JsonObject {
+					{ "Key", key },
+					{ "Values", GetValues( start.Offset[ currentRow, 1 ], DirectionType.ToRight ).ToJsonArray() }
+				};
+
+			currentRow++;
+		}
+	}
+
+	private static string[] GetValues( MSExcel.Range start, DirectionType direction )
+	{
+		var startRef = start.GetReference();
+		var endRef = startRef.End( direction );
+
+		var data =
+			Enumerable.Range( 0, direction == DirectionType.Down ? endRef.RowFirst - startRef.RowFirst + 1 : endRef.ColumnFirst - startRef.ColumnFirst + 1 )
+				.Select( i => (string)start.Offset[ direction == DirectionType.Down ? i : 0, direction == DirectionType.ToRight ? i : 0 ].Text )
+				.ToArray();
+
+		return data;
+	}
+
+	private static JsonObject GetSchedules( MSExcel.Worksheet sheet )
+	{
+		var name = sheet.Range[ "B1" ];
+		var description = sheet.Range[ "B2" ];
+		var outdoor = sheet.Range[ "A4" ];
+		var indoor = sheet.Range[ "A4" ];
+
+		while ( indoor.GetText() != "Indoor Information" && indoor.Row < 200 )
+		{
+			indoor = indoor.End[ MSExcel.XlDirection.xlDown ];
+		}
+
+		if ( indoor.GetText() != "Indoor Information" )
+		{
+			throw new ApplicationException( $"Unable to find Indoor Information on {sheet.Name}." );
+		}
+
+		return new JsonObject {
+			{ "ID", sheet.Name.Replace( "_", "." ) },
+			{ "Name", name.GetText() },
+			{ "Description", GetValues( description, DirectionType.ToRight ).ToJsonArray() },
+			{ "Indoor", GetSchedule( indoor ) },
+			{ "Outdoor", GetSchedule( outdoor ) }
+		};
+	}
+
+	private static JsonObject GetSchedule( MSExcel.Range scheduleInformation )
+	{
+		var registrationDate = scheduleInformation.Offset[ 1, 1 ].GetText();
+		var cost = scheduleInformation.Offset[ 2, 1 ].GetText();
+		var sessionsStart = scheduleInformation.Offset[ 4, 0 ];
+		var sessions = GetKeyValueArrays( sessionsStart ).ToArray();
+
+		var schedulesStart = sessionsStart.End[ MSExcel.XlDirection.xlDown ];
+		while ( schedulesStart.GetText() != "Schedules" && schedulesStart.Row < 200 )
+		{
+			schedulesStart = schedulesStart.End[ MSExcel.XlDirection.xlDown ];
+		}
+		if ( schedulesStart.GetText() != "Schedules" )
+		{
+			throw new ApplicationException( $"Unable to find schedules for {scheduleInformation.Text} on {scheduleInformation.Worksheet.Name}." );
+		}
+		var schedules = GetScheduleTables( schedulesStart.Offset[ 1 ] ).ToArray();
+
+		var notesStart = schedulesStart.End[ MSExcel.XlDirection.xlDown ];
+		while ( notesStart.GetText() != "Notes" && notesStart.Row < 200 )
+		{
+			notesStart = notesStart.End[ MSExcel.XlDirection.xlDown ];
+		}
+		if ( notesStart.GetText() != "Notes" )
+		{
+			throw new ApplicationException( $"Unable to find notes for {scheduleInformation.Text} on {scheduleInformation.Worksheet.Name}." );
+		}
+		var notes = GetValues( notesStart.Offset[ 1 ], DirectionType.Down );
+
+		return new JsonObject {
+			{ "RegistrationDate", !string.IsNullOrEmpty( registrationDate ) ? registrationDate : null },
+			{ "Cost", !string.IsNullOrEmpty( cost ) ? cost : null },
+			{ "Sessions", sessions.Length > 0 ? new JsonArray( sessions ) : null },
+			{ "Schedules", schedules.Length > 0 ? new JsonArray( schedules ) : null },
+			{ "Notes", !string.IsNullOrEmpty( notes[ 0 ] ) ? notes.ToJsonArray() : null }
+		};
+	}
+
+	private static IEnumerable<JsonObject> GetScheduleTables( MSExcel.Range scheduleStart )
+	{
+		var currentRow = 0;
+
+		while (
+			!string.IsNullOrEmpty( scheduleStart.Offset[ currentRow ].GetText() ) && // [ 0, 0 ]
+			scheduleStart.Offset[ currentRow ].GetText() != "Notes" && // [ 0, 0 ]
+			!string.IsNullOrEmpty( scheduleStart.Offset[ currentRow, 1 ].GetText() ) && // [ 0, 1 ]
+			!string.IsNullOrEmpty( scheduleStart.Offset[ currentRow + 1 ].GetText() ) // [1, 0 ]
+		)
+		{
+			var currentScheduleStart = scheduleStart.Offset[ currentRow ];
+			var rows = currentScheduleStart.End[ MSExcel.XlDirection.xlDown ].Row - currentScheduleStart.Row + 1;
+			var cols = currentScheduleStart.End[ MSExcel.XlDirection.xlToRight ].Column - currentScheduleStart.Column + 1;
+
+			yield return new JsonObject {
+				{ "ID", currentScheduleStart.GetText() },
+				{ "Rows", 
+					Enumerable.Range( 0, rows )
+						.Select( r => 
+							Enumerable.Range( 0, cols )
+								.Select( c => scheduleStart.Offset[ currentRow + r, c ].GetText() )
+								.ToJsonArray() 
+						)
+						.ToJsonArray()
+				}
+			};
+
+			currentRow += rows + 1;
+		}
+	}
+
+	private static IEnumerable<JsonKeyProperty> GetPropertyValueArrays( MSExcel.Range start )
+	{
+		var currentRow = 0;
+		string key;
+
+		while ( !string.IsNullOrEmpty( key = start.Offset[ currentRow ].GetText() ) )
+		{
+			yield return
+				new JsonKeyProperty (
+					key.Replace( " ", "" ),
+					GetValues( start.Offset[ currentRow, 1 ], DirectionType.ToRight ).ToJsonArray()
+				);
+
+			currentRow++;
+		}
+	}
+}
--- src/Ribbon.Handlers.ConfigurationExporting.cs
+++ src/Ribbon.Handlers.ConfigurationExporting.cs
@@ -1,20 +1,7 @@
-﻿using MSExcel = Microsoft.Office.Interop.Excel;
-using ExcelDna.Integration;
+﻿using ExcelDna.Integration;
 using ExcelDna.Integration.CustomUI;
-using System.Text.Json.Nodes;
-using KAT.Camelot.RBLe.Core.Calculations;
-using KAT.Camelot.Domain.Extensions;
-using System.Globalization;
 
 namespace KAT.Camelot.Extensibility.Excel.AddIn;
-
-enum LookupConfigurationType
-{
-	DataTables,
-	FrameworkTables,
-	RateTables,
-	GlobalTables
-}
 
 public partial class Ribbon
 {
@@ -28,7 +15,11 @@ public partial class Ribbon
 		}
 		else if ( WorkbookState.IsRTCFile )
 		{
-			MessageBox.Show( "// TODO: Export RTCFile" );
+			ExcelAsyncUtil.QueueAsMacro( () =>
+			{
+				ExportRtcData();
+				MessageBox.Show( "RTC Data Exported" );
+			} );
 		}
 		else if ( WorkbookState.IsSpecSheetFile )
 		{
@@ -48,181 +39,4 @@ public partial class Ribbon
 
 	public void ConfigurationExporting_ExportSheet( IRibbonControl _ ) =>
 		ExportGlobalTables( currentSheet: !WorkbookState.IsGlobalTablesFile );
-	
-
-	private void ExportGlobalTables( string? downloadName = null, bool currentSheet = false )
-	{
-		var owner = new NativeWindow();
-		owner.AssignHandle( new IntPtr( application.Hwnd ) );
-
-		RunRibbonTask( async () =>
-		{
-			using var processGlobalTables = new ProcessGlobalTables( 
-				currentSheet,
-				AddIn.Settings.DataServices,
-				GetWindowConfiguration( nameof( ProcessGlobalTables ) )				
-			);
-
-			var info = processGlobalTables.GetInfo( 
-				AddIn.Settings.KatUserName, 
-				await AddIn.Settings.GetClearPasswordAsync(),
-				owner
-			);
-
-			if ( info == null ) return;
-
-			ExcelAsyncUtil.QueueAsMacro( () => application.Cursor = MSExcel.XlMousePointer.xlWait );
-
-			SaveWindowConfiguration( nameof( ProcessGlobalTables ), info.WindowConfiguration );
-			
-			await UpdateAddInCredentialsAsync( info.UserName, info.Password );
-
-			await DownloadLatestCalcEngineAsync( downloadName );
-
-			SetStatusBar( "Processing Global Tables..." );
-
-			ExcelAsyncUtil.QueueAsMacro( () =>
-			{
-				application.Cursor = MSExcel.XlMousePointer.xlWait;
-				var existing = currentSheet ? application.ActiveWorkbook : application.GetWorkbook( Constants.FileNames.GlobalTables )!;
-
-				var sheets = existing.Worksheets.Cast<MSExcel.Worksheet>().ToDictionary( x => x.Name );
-				var resourceTable = sheets.TryGetValue( Constants.SpecSheet.TabNames.Localization, out var l )
-					? l.RangeOrNull( Constants.SpecSheet.RangeNames.ResourceTable )
-					: null;
-
-				var globalSpecifications = new JsonObject();
-
-				static JsonObject getGlobalTables( MSExcel.Worksheet worksheet )
-				{
-					var tables = GetGlobalLookupTables( worksheet ).ToJsonArray();
-					var version = worksheet.RangeOrNull<string>( Constants.SpecSheet.RangeNames.SheetVersion );
-
-					return new JsonObject
-					{
-						{ "version", version },
-						{ "tables", tables }
-					};
-				}
-
-				if ( currentSheet )
-				{
-					var worksheet = application.ActiveWorksheet();
-					
-					var propertyName = worksheet.RangeOrNull<string>( Constants.SpecSheet.RangeNames.SheetType ) == Constants.SpecSheet.SheetTypes.GlobalLookupTables
-						? "dataTables"
-						: "rateTable";
-
-					globalSpecifications[ propertyName ] = getGlobalTables( worksheet );
-				}
-				else
-				{
-					globalSpecifications[ "dataTables" ] = getGlobalTables( sheets[ Constants.SpecSheet.TabNames.DataLookupTables ] );
-					globalSpecifications[ "rateTables" ] = getGlobalTables( sheets[ Constants.SpecSheet.TabNames.RateTables ] );
-				}
-
-				RunRibbonTask( async () => {
-					var validations = await apiService.UpdateGlobalTablesAsync( info.ClientName, info.Targets, globalSpecifications, info.UserName, info.Password );
-
-					if ( validations != null )
-					{
-						ShowValidations( validations );
-						return;
-					}
-
-					ExcelAsyncUtil.QueueAsMacro( () =>
-					{
-						MessageBox.Show( owner, $"Successfully updated Global Tables on following environments.{Environment.NewLine + Environment.NewLine + string.Join( ", ", info.Targets )}", "Update Global Tables" );
-						
-						if ( downloadName != null )
-						{
-							existing.Close( SaveChanges: false );
-						}
-					} );
-				}, "ExportGlobalTables (Processing)" );
-			} );
-		} );
-	}
-
-	private static IEnumerable<JsonObject> GetGlobalLookupTables( MSExcel.Worksheet worksheet )
-	{
-		var configurationType = worksheet.RangeOrNull<string>( Constants.SpecSheet.RangeNames.SheetType ) switch
-		{
-			Constants.SpecSheet.SheetTypes.GlobalLookupTables => LookupConfigurationType.GlobalTables,
-			Constants.SpecSheet.SheetTypes.GlobalRateTables or Constants.SpecSheet.SheetTypes.ClientRateTables => LookupConfigurationType.RateTables,
-			_ => LookupConfigurationType.DataTables // TODO: Is this right?
-		};
-
-		var headerOffset = configurationType == LookupConfigurationType.DataTables ? 2 : 1;
-
-		var firstColumn = 
-			worksheet.Range[ worksheet.RangeOrNull<string>( Constants.SpecSheet.RangeNames.TableStartAddress ) ]
-				.GetReference()
-				.Offset( headerOffset, 0 );
-
-		while ( !string.IsNullOrEmpty( firstColumn.GetText() ) )
-		{
-			var tableName = firstColumn.Offset( -headerOffset, 1 ).GetText()!;
-			var lastColumn = firstColumn.End( DirectionType.ToRight );
-			var tableInclude = firstColumn.Offset( -1, 1 ).GetText() ?? "Y";
-
-			if ( configurationType != LookupConfigurationType.DataTables || tableInclude.StartsWith( "Y" ) )
-			{
-				var lastRow = firstColumn.End( DirectionType.Down );
-				var data =
-					firstColumn
-						.Extend( lastColumn )
-						.Extend( lastRow )
-						.GetValueArray();
-
-				if ( tableInclude.Contains( "/customize" ) )
-				{
-					throw new NotImplementedException( $"No support for /customize. {tableName} used this flag.  See if it is needed." );
-				}
-
-				yield return new JsonObject
-				{
-					{ "name", tableName },
-					{ "columns", new JsonArray().AddItems( data.Rows.First() ) },
-					{ "rows", data.Rows.Skip( 1 ).Select( r => new JsonArray().AddItems( r.Select( GetExportValue ), includeNulls: true ) ).ToJsonArray() }
-				};
-			}
-
-			firstColumn = lastColumn.End( DirectionType.ToRight, ignoreEmpty: true );
-		}
-	}
-
-	private static string? GetExportValue( object value )
-	{
-		if ( value == ExcelEmpty.Value ) return null;
-
-		var d = value as double?;
-		if ( d != null )
-		{
-			// .NET Core changed in the underlying implementation of the Double.ToString() method. 
-			// In .NET Core, the method has been updated to produce a round-trippable result by default, 
-			// which means it will always return a string that, when parsed, will produce the original number.
-			// To reproduce what we had in .NET Framework it is suggested to use the G15 format.  
-			// The "G" format specifier stands for "general", and it formats the number in the most compact, human-readable form.
-			// In .NET Framework, the default precision for double.ToString() without any format specifier is up to 15 digits, 
-			// which can be either to the left or right of the decimal point. This means it can include up to 15 significant digits, 
-			// and the remaining digits are replaced with zeros.
-			return d.Value.ToString( "G15", CultureInfo.InvariantCulture );
-
-			// I was going to count the number of decimal places and use that + 6, but it seems to be a bit overkill.
-			// var decimalValues = ( (int)Math.Floor( Math.Abs( d.Value ) ) ).ToString().Length;
-			// return d.Value.ToString( $"G{decimalValues + 6}", CultureInfo.InvariantCulture );
-		}
-
-		var dt = value as DateTime?;
-		if ( dt != null ) return dt.Value.ToString( "yyyy-MM-dd" );
-
-		var s = (string)value;
-
-		// Excel does all caps for these.
-		if ( s == "TRUE" ) s = "true";
-		else if ( s == "FALSE" ) s = "false";
-
-		return s;
-	}
 }
